85d68e380cc1f1ff060bbabbaf12f4d9945a9139
Nejuf <nejuf1@gmail.com>
2017-02-24 07:05:56 +0000

    Git: Adds default push setting and makes user info dynamic

--- install_scripts/install_git.sh
+++ install_scripts/install_git.sh
@@ -2,8 +2,17 @@
 
 # Installs and configures git on Raspberry Pi
 
-git config --global color.ui true
-git config --global user.name Nejuf
-git config --global user.email nejuf1@gmail.com
+echo "Enter Git Username:"
+read username
+echo "Enter Git Email:"
+read email
 
+# Git comes pre-installed with Raspbian
+
+# Configure Git
+git config --global color.ui true
+git config --global push.default simple
+
+git config --global user.name "$username"
+git config --global user.email "$email"
 
